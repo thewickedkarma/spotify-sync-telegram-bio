dc65ff425b3c99c849ef3face14bf747249ac09f
Levi Hackerman <62335141+thewickedkarma@users.noreply.github.com>
2023-02-10 14:05:54 +0530

    Update update.py

--- update.py
+++ update.py
@@ -16,7 +16,7 @@ uid="paste your uid"
 
 
 
-client=TelegramClient('user',api_id, api_hash)
+client=TelegramClient('tguser',api_id, api_hash)
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0'}
 
